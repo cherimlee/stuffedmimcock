6042d622d07a4401ba007361801fe43dcf77feb2
cherimlee <coflacoruddl@naver.com>
2020-12-27 15:22:16 +0900

    id,password

--- src/study/Id.java
+++ src/study/Id.java
@@ -4,13 +4,13 @@ public class Id {
 
 	public static void main(String[] args) {
 		String id = args[0];
+		String password = args[1];
 		if (id.equals("egoing")) {
+			if (password.equals("1111")) {
 			System.out.println("right");
-		}
-		else {
+		}	else  {
 			System.out.println("wrong");
 		}
 		
-	}
-
+	
 }
